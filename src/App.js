2811a241a7bbc33cff5ffb94cef8b475b02f0436
Aleynaozc <aleynaozcelik48@gmail.com>
2022-05-10 18:23:23 +0300

    change-image-file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TodoModal from './components/TodoModal';
 //Alert
 import { Toaster } from 'react-hot-toast';
 //Image
-import TodoAppBg from './images/bg.jpeg';
+
 //Fontawesome//
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
@@ -19,7 +19,7 @@ function App() {
       <div className="App">
         <div className="container ">
           <div className="header">
-            <img src={TodoAppBg} />
+            <img src="/image/bg.jpeg" />
             <span className="title">TODO APP</span>
           </div>
           <div className='buttons' >
@@ -30,7 +30,7 @@ function App() {
           <button type="button" onClick={() => setModalOpen(true)} //Buttona basıldığında modal açılıyor.
             className="open-modal-btn"
           >
-           <FontAwesomeIcon icon={faPenToSquare} />
+            <FontAwesomeIcon icon={faPenToSquare} />
           </button>
           <TodoModal type="save" modalOpen={modalOpen} setModalOpen={setModalOpen} />
           <Toaster toastOptions={{
